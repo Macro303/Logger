8305eb56c07da43d93fdcbe9a4ee9ab508b45923
Macro303 <Macro303@users.noreply.github.com>
2020-09-30 09:38:19 +1300

    Update to include variables

--- __init__.py
+++ __init__.py
@@ -2,23 +2,24 @@
 import logging
 from pathlib import Path
 
-from coloured_formatter import ColouredFormatter
+from Logger.coloured_formatter import ColouredFormatter
 
 TOP_DIR = Path(__file__).resolve().parent.parent
 LOG_DIR = TOP_DIR.joinpath('logs')
 
 
-def init_logger(project: str) -> None:
+def init_logger(project: str, file_level = logging.DEBUG, console_level = logging.INFO, show_console: bool = True) -> None:
     root = logging.getLogger()
     logger = logging.getLogger(__name__)
 
-    stream_logger = logging.StreamHandler()
-    stream_logger.setLevel(logging.INFO)
-    coloured_formatter = ColouredFormatter(
-        fmt='[%(asctime)s] [%(levelname)-8s] {%(name)s:%(filename)s} | %(message)s'
-    )
-    stream_logger.setFormatter(coloured_formatter)
-    root.addHandler(stream_logger)
+    if show_console:
+        stream_logger = logging.StreamHandler()
+        stream_logger.setLevel(logging.INFO)
+        coloured_formatter = ColouredFormatter(
+            fmt='[%(asctime)s] [%(levelname)-8s] {%(name)s:%(filename)s} | %(message)s'
+        )
+        stream_logger.setFormatter(coloured_formatter)
+        root.addHandler(stream_logger)
 
     LOG_DIR.mkdir(exist_ok=True)
     file_logger = logging.FileHandler(filename=LOG_DIR.joinpath(f"{project}.log"), encoding='UTF-8')
@@ -29,7 +30,7 @@ def init_logger(project: str) -> None:
     file_logger.setFormatter(formatter)
     root.addHandler(file_logger)
 
-    root.setLevel(logging.DEBUG)
+    root.setLevel(logging.NOTSET)
     logger.log(logging.NOTSET, 'NOTSET is Visible')
     logger.log(logging.DEBUG, 'DEBUG is Visible')
     logger.log(logging.INFO, 'INFO is Visible')
