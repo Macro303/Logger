a67f99afb335c640729ab1312123443f9fdb2b54
Macro303 <Macro303@users.noreply.github.com>
2021-03-30 09:33:09 +1300

    Swap to Colorama variables instead of ansi

--- Logger.py
+++ Logger.py
@@ -3,7 +3,7 @@ import logging
 from logging import Formatter
 from pathlib import Path
 
-from colorama import init
+from colorama import init, Fore, Style
 
 TOP_DIR = Path(__file__).resolve().parent.parent
 LOG_DIR = TOP_DIR.joinpath('logs')
@@ -11,19 +11,20 @@ LOG_DIR = TOP_DIR.joinpath('logs')
 
 class ColouredFormatter(Formatter):
     MAPPING = {
-        logging.DEBUG: 32,  # Green
-        logging.INFO: 37,  # White
-        logging.WARNING: 33,  # Yellow
-        logging.ERROR: 31,  # Red
-        logging.CRITICAL: 35,  # Magenta
+        logging.DEBUG: Fore.GREEN,
+        logging.INFO: Style.RESET_ALL,
+        logging.WARNING: Fore.YELLOW,
+        logging.ERROR: Fore.RED,
+        logging.CRITICAL: Fore.MAGENTA
     }
-    PREFIX = '\033['
-    SUFFIX = '\033[0m'
+    # PREFIX = '\033['
+    # SUFFIX = '\033[0m'
     FORMAT = '[%(asctime)s] [%(levelname)-8s] {%(name)s} | %(message)s'
 
     def format(self, record):
-        seq = self.MAPPING.get(record.levelno, 37)  # Default INFO
-        log_fmt = '{0}{1}m{2}{3}'.format(self.PREFIX, seq, self.FORMAT, self.SUFFIX)
+        seq = self.MAPPING.get(record.levelno, Style.RESET_ALL)  # Default INFO
+        # log_fmt = '{0}{1}m{2}{3}'.format(self.PREFIX, seq, self.FORMAT, self.SUFFIX)
+        log_fmt = f"{seq}{self.FORMAT}"
         formatter = logging.Formatter(fmt=log_fmt, datefmt='%Y-%m-%d %H:%M:%S')
         return formatter.format(record)
 
